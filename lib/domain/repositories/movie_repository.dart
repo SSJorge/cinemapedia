acdb71cfc011c392470f94f940d95e61b1c89776
Jorge Moreno <jorge.morenot@sansano.usm.cl>
2026-02-27 20:45:56 -0300

    first commit

---
+++ lib/domain/repositories/movie_repository.dart
@@ -0,0 +1,9 @@
+import 'package:cinemapedia/domain/entities/movie.dart';
+
+abstract class MovieRepository {
+  Future<List<Movie>> getNowPlaying({int page = 1});
+}
+
+//nuestros origenes de datos son los datasource y los repos son quienes van a llamar al datasource
+//llamamos al datasource a traves del repo pq el repo es el q me apermitir a mi cambiar el datasource
+// por ej quiero moviedb, ibmdb, de cualquier lugar podremos cambiar el origenes de datos facilmente
