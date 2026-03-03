0632b03e1c53726d0d7c053b9be9e8fc070590df
Jorge Moreno <jorge.morenot@sansano.usm.cl>
2026-03-03 17:45:26 -0300

    barra busqueda

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,5 +1,7 @@
-import 'package:cinemapedia/presentation/providers/providers.dart';
 import 'package:flutter/material.dart';
+
+import 'package:cinemapedia/presentation/providers/providers.dart';
+import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -41,12 +43,21 @@ class _HomeViewState extends ConsumerState<_HomeView> {
       return const Center(child: CircularProgressIndicator());
     }
 
-    return ListView.builder(
-      itemCount: nowPlayingMovies.length,
-      itemBuilder: (context, index) {
-        final movie = nowPlayingMovies[index];
-        return ListTile(title: Text(movie.title));
-      },
+    return Column(
+      children: [
+        CustomAppbar(),
+
+        Expanded(
+          //dado el padre expanda todo lo posible
+          child: ListView.builder(
+            itemCount: nowPlayingMovies.length,
+            itemBuilder: (context, index) {
+              final movie = nowPlayingMovies[index];
+              return ListTile(title: Text(movie.title));
+            },
+          ),
+        ),
+      ],
     );
   }
 }
---
+++ lib/presentation/widgets/shared/custom_appbar.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class CustomAppbar extends StatelessWidget {
+  const CustomAppbar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    final textStyles = Theme.of(context).textTheme.titleMedium;
+
+    return SafeArea(
+      bottom: false,
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 10),
+        child: SizedBox(
+          width: double.infinity,
+          child: Row(
+            children: [
+              Icon(Icons.movie_outlined, color: colors.primary),
+              const SizedBox(width: 5),
+              Text('Cinemapedia', style: textStyles),
+
+              Spacer(),
+
+              IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
